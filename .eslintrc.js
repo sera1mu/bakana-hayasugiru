dcc30af22aa57e43926d9aecac795ea56402b641
Seraimu <seraimu.neko@outlook.jp>
2022-01-26 16:17:01 +0900

    chore(eslintrc): Add parserOptions.project

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,8 @@ module.exports = {
     ],
     "parser": "@typescript-eslint/parser",
     "parserOptions": {
-        "ecmaVersion": "latest"
+        "ecmaVersion": "latest",
+        "project": "./tsconfig.json"
     },
     "plugins": [
         "@typescript-eslint"
